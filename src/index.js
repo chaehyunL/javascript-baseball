bc7256e781215b2fb69071b0e19356533f286d01
chaehyunL <chaehyun1012@gmail.com>
2026-09-14 10:46:29 +0900

    feat: index.js 생성

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import BaseballGame from "./BaseBallGame.js";
+import ComputerNumber from "./ComputerNumber.js";
+import InputValidation from "./InputValidation.js";
+
+const baseBallGame = new BaseballGame();
+const computerNumber = new ComputerNumber();
+const inputValidation = new InputValidation();
+
+const answerNumbers = computerNumber.getNumber();
+
+const userInput = document.querySelector("#user-input");
+const result = document.querySelector("#result");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (inputValidation.isValid(userInput.value)) {
+        const userInputNumbers = userInput.value
+            .split("").map(Number);
+
+        const gameResult = baseBallGame.play(answerNumbers, userInputNumbers);
+
+        result.textContent = gameResult;
+    }
+    else {
+        window.alert(["잘못된 값을 입력하였음."]);
+    }
+});
+
